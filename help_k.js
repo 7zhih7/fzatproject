6ef595a3a43ed3ca4a679f06b0e983856e496b73
Mihail <113715475+7zhih7@users.noreply.github.com>
2023-03-07 01:01:47 +0300

    Add files via upload

--- help_k.js
+++ help_k.js
@@ -1,11 +1,11 @@
 function myFunction_help_k() {
-    var x = document.getElementById("myForm");
+    var x = document.getElementById("myForm_help_k");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
     }
   }
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+  function closeForm_help_k() {
+    document.getElementById("myForm_help_k").style.display = "none";
 }
